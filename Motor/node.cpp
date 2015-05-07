7afd23344bb4802369f37718a89572b82f943437
gus3000 <gus3000spam@gmail.com>
2015-05-07 10:51:04 +0200

    Beginning node.cpp

--- Motor/node.cpp
+++ Motor/node.cpp
@@ -1,12 +1,19 @@
 #include "node.hpp"
 
-Node::Node()
+template <typename StateType>
+Node<StateType>::Node(StateType s)
 {
 
 }
 
-Node::~Node()
+template <typename StateType>
+Node<StateType>::~Node()
 {
+    //we delete every child
+    for(auto it = children.begin(); it != children.end(); it++)
+    {
+        delete *it;
+    }
 
+    //note that we did not delete the parent node, so if you want to destroy the whole tree you want to call delete on root.
 }
-
--- Motor/node.hpp
+++ Motor/node.hpp
@@ -1,11 +1,22 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
+#include <vector>
 
+using std::vector;
+
+template <typename StateType>
 class Node
 {
+private:
+    const Node* parent;
+    const int depth;
+    StateType state;
+    double f,g,h; //f = g+h
+    vector<Node *> children;
+
 public:
-    Node();
+    Node(StateType s);
     ~Node();
 };
 
